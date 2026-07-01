a35baf707c6087c4c35e5675db09efd293fa542e
Paanyaa <praneshpatil28@gmail.com>
2026-07-01 09:15:06 +0530

    Done

--- hyprland.lua
+++ hyprland.lua
@@ -6,7 +6,7 @@
 hl.env("GBM_BACKEND",                "nvidia-drm")
 hl.env("__GLX_VENDOR_LIBRARY_NAME",  "nvidia")
 hl.env("LIBVA_DRIVER_NAME",          "nvidia")
-hl.env("AQ_DRM_DEVICES",            "/dev/dri/card1:/dev/dri/card0") -- Nvidia first, Intel fallback
+hl.env("WLR_DRM_DEVICES",            "/dev/dri/card1:/dev/dri/card0") -- Nvidia first, Intel fallback
 
 hl.env("XDG_SESSION_TYPE",             "wayland")
 hl.env("XDG_CURRENT_DESKTOP",          "Hyprland")
@@ -104,4 +104,3 @@ hl.config({
 })
 end
 
-require("hyprland.env")
